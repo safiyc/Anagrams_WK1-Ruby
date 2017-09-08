f1b3fcfcd5042d51ca087cfad2187d2528252a7f
Safiy Cham <csafi13@yahoo.com>
2017-09-08 14:53:58 -0700

    add find vowels to confirm if real words.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -5,19 +5,29 @@ class Words
     @words = words
   end
 
+  #check for letters
   def check_alphas (new_words)
     new_words = @words
     if @words =~ /^[a-zA-Z]+$/
       true
     else
-      false
+      "Please enter only letters."
     end
   end
 
+  #rule 2: change to lowercase
   def to_lowercase (new_words)
     new_words = @words
     @words.downcase
   end
 
-
+  #rule 4: check for vowels to confirm are words
+  def find_vowels (new_words)
+    new_words = @words
+    if @words =~ /[aeiou]/
+      "It's a real word!"
+    else
+      "Please enter vowels to spell real words"
+    end
+  end
 end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -3,14 +3,18 @@ require('anagrams_antigrams')
 require('pry')
 
 describe('#anagrams_antigrams') do
-  it("Check if string has non-alphas") do
+  it("Check if input has non-alphas") do
     alpha_words = Words.new("hello")
     expect(alpha_words.check_alphas("hello")).to(eq(true))
   end
 
-  it("Change all letter casing to lowercase") do
+  it("Change input to lowercase") do
     lowercase_words = Words.new("Hello")
     expect(lowercase_words.to_lowercase("Hello")).to(eq("hello"))
   end
 
+  it("Check if input has vowels to confirm real words") do
+    real_words = Words.new("Bird")
+    expect(real_words.find_vowels("Bird")).to(eq("It's a real word!"))
+  end
 end
