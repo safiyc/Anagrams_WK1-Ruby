f59074ee9729e5a181f9e34786fa120531a6faad
Safiy Cham <csafi13@yahoo.com>
2017-09-13 19:03:02 -0700

    add passing unit test to confirm inputs are reals words.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,8 +1,17 @@
 require('pry')
 
 class Words
-  def initialize (words)
-    @words = words
+  def initialize (word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+
+  def confirm_anagrams
+    if @word1 =~ /[aeiouy]/ && @word2 =~ /[aeiouy]/
+      return "These are real words."
+    else
+      return "These are not real words because they don't have vowels."
+    end
   end
 
 #   def find_anagrams (new_words)
@@ -15,32 +24,32 @@ class Words
 #   end
 # end
 
-  def check_alphas (new_words)
-    new_words = @words
-    if @words =~ /^[a-zA-Z]+$/
-      true
-    else
-      "Please enter only letters."
-    end
-  end
-
-  def to_lowercase (new_words)
-    new_words = @words
-    @words.downcase
-  end
-
-  def find_vowels (new_words)
-    new_words = @words
-    if @words =~ /[aeiou]/
-      "It's a real word!"
-    else
-      "Please include vowels to make words real."
-    end
-  end
-
-  def check_anagrams (new_words)
-    new_words.chars.sort.join == @words.chars.sort.join
-  end
+  # def check_alphas (new_words)
+  #   new_words = @words
+  #   if @words =~ /^[a-zA-Z]+$/
+  #     true
+  #   else
+  #     "Please enter only letters."
+  #   end
+  # end
+  #
+  # def to_lowercase (new_words)
+  #   new_words = @words
+  #   @words.downcase
+  # end
+  #
+  # def find_vowels (new_words)
+  #   new_words = @words
+  #   if @words =~ /[aeiou]/
+  #     "It's a real word!"
+  #   else
+  #     "Please include vowels to make words real."
+  #   end
+  # end
+  #
+  # def check_anagrams (new_words)
+  #   new_words.chars.sort.join == @words.chars.sort.join
+  # end
 
   #combine above methods
   # def methods_combined (new_words)
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -3,23 +3,30 @@ require('anagrams_antigrams')
 require('pry')
 
 describe('#anagrams_antigrams') do
-  it("Check if input has non-alphas") do
-    alpha_words = Words.new("Hello")
-    expect(alpha_words.check_alphas("Hello")).to(eq(true))
-  end
-
-  it("Change input to lowercase") do
-    lowercase_words = Words.new("Hello")
-    expect(lowercase_words.to_lowercase("Hello")).to(eq("hello"))
-  end
-
-  it("Check if input has vowels to confirm real words") do
-    real_words = Words.new("bird")
-    expect(real_words.find_vowels("bird")).to(eq("It's a real word!"))
-  end
-
-  it("Sort input into an anagram") do
-    anagram_words = Words.new("racecar")
-    expect(anagram_words.check_anagrams("racecar")).to(eq("racecar"))
+  it("checks for vowels to confirm if real words") do
+    user_words = Words.new("rbd", "dbr")
+    expect(user_words.confirm_anagrams()).to(eq("These are not real words because they don't have vowels."))
   end
 end
+
+# describe('#anagrams_antigrams') do
+#   it("Check if input has non-alphas") do
+#     alpha_words = Words.new("Hello")
+#     expect(alpha_words.check_alphas("Hello")).to(eq(true))
+#   end
+#
+#   it("Change input to lowercase") do
+#     lowercase_words = Words.new("Hello")
+#     expect(lowercase_words.to_lowercase("Hello")).to(eq("hello"))
+#   end
+#
+#   it("Check if input has vowels to confirm real words") do
+#     real_words = Words.new("bird")
+#     expect(real_words.find_vowels("bird")).to(eq("It's a real word!"))
+#   end
+#
+#   it("Sort input into an anagram") do
+#     anagram_words = Words.new("racecar")
+#     expect(anagram_words.check_anagrams("racecar")).to(eq("racecar"))
+#   end
+# end
