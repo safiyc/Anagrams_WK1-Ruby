b7799c990207039462e54d45db3d0bb0e31ccb05
Safiy Cham <csafi13@yahoo.com>
2017-09-17 16:34:31 -0700

    add passing spec test to check inputs for vowels to confirm if they are real words.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -16,4 +16,10 @@ describe('#anagrams_antigrams') do
    expect(word1.input_value).to(eq("ruby"))
    expect(word2.input_value).to(eq("bury"))
  end
+
+ it("checks for vowels to confirm if real words") do
+   word1 = Word.new("ruby")
+   word2 = Word.new("rbd")
+   expect(word1.confirm_anagrams(word2)).to(eq("One or two of these inputs are not real words. Real words have vowels."))
+ end
 end
